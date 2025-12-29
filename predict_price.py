12fca88c20f44c4b9271f0b2fd5d660ba4627fb9
kalum-gunawardhana <kguna415@gmail.com>
2025-12-29 12:26:19 +0530

    Add dataset loading and preprocessing in predict_price.py

--- predict_price.py
+++ predict_price.py
@@ -3,3 +3,11 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+
+# load dataset
+data = pd.read_csv("Mobile_Price_Classification-220531-204702.csv")
+
+x = data.drop("price_range", axis=1)
+
+scaler = StandardScaler()
+scaler.fit(x)
