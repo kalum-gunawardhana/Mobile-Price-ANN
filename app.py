de683e1fdfe918c0ff078e18e3e828d9040bde41
kalum-gunawardhana <kguna415@gmail.com>
2025-12-29 09:32:05 +0530

    Remove unused imports from app.py and add .gitignore for virtual environment

--- app.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
